9f7cf41dc8b581991784e009489e6e3b84a01648
尹派 <yinpai@89trillion.com>
2021-11-04 11:49:45 +0800

    feat: Http 实现http请求

---
+++ Assets/Scripts/Controller/CallBack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallBack : MonoBehaviour
+{
+    //单例
+    private static CallBack instance;
+    public static CallBack Instance()
+    {
+        return instance;
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public JsonController jiexi;
+
+    //绑定回调函数
+    public void RequestRankList(int type, int page, int season)
+    {
+        SendHttpRequest api = new SendHttpRequest(gameObject);
+        api.OnSuccess = data => { OnRankApiSuccess(data); };
+        api.Request(type, page, season);
+    }
+
+    private void OnRankApiSuccess(string data)
+    {
+        //读取在服务器获取的数据
+        jiexi.ReadDatainServer(data);
+    }
+
+
+}
